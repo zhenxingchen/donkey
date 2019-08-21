75112faedd673e14716e4d7380961cfbecc19cfc
zhenxing.chen <zhenxing.chen@luckincoffee.com>
2019-08-21 10:37:52 +0800

    主文件调整

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,17 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import log from "./shared/log";
 import Donkey from "./components/donkey";
-
-import "./style/base.less";
-import "./style/layout.less";
-import "./style/space.less";
-import "./style/media.less";
-import "./style/form.less";
-import "./style/transition.less";
-// import "./style/webkit.less";
+import "./style/index.less";
 
 const dk = {
   start: (config: any, id: string = "donkey") => {
     if (!config) {
       log.error("please set your config");
       return ;
+    }
+    const target = document.getElementById(id);
+    if (!target) {
+      log.error("donkey render target is not exist, please make sure you had set the ${id}");
     }
     log.debug(`donkey will render on the ${id}`);
     ReactDOM.render(
